6e076a58987491814c94c247908b3ee5fd089c13
agustibayu <agustibayu@refactory.id>
2026-09-23 20:31:06 +0700

    menambah transisi pindah halaman login ke register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,9 +7,9 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="page-transition">
     <div class="flex min-h-screen justify-center items-center bg-[#FFFFFF]">
-        <form action="#" class="shadow-md hover:scale-105 transaction duration-300 flex gap-2 p-2">
+        <form action="#" class="rounded-lg shadow-md hover:scale-105 transaction duration-300 flex gap-2 p-2 border-1">
             <img src="{{asset('images/login_photo.jpg')}}" alt="Login" class="h-75 w-75">
             <div class="flex flex-col gap-2 items-stretch justify-center">
                 <span>Login Page</span>
@@ -17,6 +17,7 @@
                 <input type="text" class="border-1">
                 <button class="bg-blue-500 w-auto h-auto">Submit</button>
                 <button class="bg-[#000000] text-[#ffffff] w-auto h-auto">google</button>
+                <a href="{{route('register')}}" class="hover:underline text-blue-600 text-xs">Belum punya akun? Daftar</a>
             </div>
         </form>
     </div>
